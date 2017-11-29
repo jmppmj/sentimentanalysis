045ae970f08a42d6d4c691f0f867802bd9f613b1
jmppmj <jillplatts@gmail.com>
2017-11-28 22:05:32 -0500

    Add files via upload

---
+++ RedditbotSpider/RedditbotSpider/items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Define the models for your scraped items here
+
+from scrapy.item import Item, Field
+import scrapy
+
+
+class RedditscrapyItem(scrapy.Item):
+#define the fields for your item here like:
+#name = scrapy.Field()
+ 
+    pass
